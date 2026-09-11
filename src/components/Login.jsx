37192457561277612ef5e2cd01edc83f20f0d0d6
Uvissh <vishalbhateria@gmail.com>
2026-09-11 13:09:29 +0530

    changes in url3

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -29,7 +29,7 @@ function Login({ setIsLoggedIn, setUsername }) {
 
         try {
 
-            const response = await api.post(
+            const response = await axios.post(
                 "https://eventbookingbackend-dhdr.onrender.com/api/auth/login",
                 {
                     email: email,
